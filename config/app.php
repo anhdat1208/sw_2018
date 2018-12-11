985a6632f5c86615400ecc9cd2322fdd3c285621
codeatoz <thquan.2510@gmail.com>
2018-12-11 17:33:23 +0700

    Commit feature/localization branch

--- config/app.php
+++ config/app.php
@@ -99,28 +99,28 @@ return [
     /**
      * Settings for Localization
      */
-    'default_locale' => config('app.fallback_locale'),
-    'supported_locales' => [
-        'en' => [
-            'native' => 'English',
-            'regional_code' => 'en_GB',
-            'charset' => 'UTF-8',
-            'constants' => ['LC_TIME'],
-        ],
-        'fr' => [
-            'native' => 'Français',
-            'regional_code' => 'fr_FR',
-            'charset' => 'UTF-8',
-            'constants' => ['LC_TIME'],
-        ],
-        'vi' => [
-            'native' => 'Tiếng Việt',
-            'regional_code' => 'vi_VN',
-            'charset' => 'UTF-8',
-            'constants' => ['LC_ALL'],
-        ],
-    ],
-    'hide_default_locale_in_url' => true,
+    // 'default_locale' => config('app.fallback_locale'),
+    // 'supported_locales' => [
+    //     'en' => [
+    //         'native' => 'English',
+    //         'regional_code' => 'en_GB',
+    //         'charset' => 'UTF-8',
+    //         'constants' => ['LC_TIME'],
+    //     ],
+    //     'fr' => [
+    //         'native' => 'Français',
+    //         'regional_code' => 'fr_FR',
+    //         'charset' => 'UTF-8',
+    //         'constants' => ['LC_TIME'],
+    //     ],
+    //     'vi' => [
+    //         'native' => 'Tiếng Việt',
+    //         'regional_code' => 'vi_VN',
+    //         'charset' => 'UTF-8',
+    //         'constants' => ['LC_ALL'],
+    //     ],
+    // ],
+    // 'hide_default_locale_in_url' => true,
 
     /*
     |--------------------------------------------------------------------------
